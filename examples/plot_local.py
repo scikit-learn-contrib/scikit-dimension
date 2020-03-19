6c5e7a96da808beb823321cc41802b576fda626b
Jonathan Bac <36225255+j-bac@users.noreply.github.com>
2020-03-19 11:29:58 +0100

    Update plot_local.py

--- examples/plot_local.py
+++ examples/plot_local.py
@@ -3,10 +3,8 @@
 Local ID example
 ===========================
 """
-import skdim
-from skdim import local_id
+from skdim import local_id, asPointwise
 import numpy as np
-from matplotlib import pyplot as plt
 
 X = np.random.random((1000,10))
 
@@ -21,6 +19,6 @@ lpca = local_id.lPCA().fit(X)
 
 
 #all datapoint neighborhoods
-pw_id = skdim.asPointwise(X,local_id.FisherS().fit,n_neighbors=100,n_jobs=1)
+pw_id = asPointwise(X,local_id.FisherS().fit,n_neighbors=100,n_jobs=1)
 
 
