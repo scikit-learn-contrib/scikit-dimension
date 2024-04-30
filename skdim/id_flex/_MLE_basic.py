08c0e8299484f4bfad60efb0d748677b3708ec53
KM Yim <32544571+kmyim@users.noreply.github.com>
2024-04-30 14:52:18 +0100

    updated mle_basic to use radial dists from nbhds

--- skdim/id_flex/_MLE_basic.py
+++ skdim/id_flex/_MLE_basic.py
@@ -5,52 +5,46 @@ from .._commonfuncs import FlexNbhdEstimator
 from sklearn.metrics import DistanceMetric
 
 class MLE_basic(FlexNbhdEstimator):
+    '''
+    Basic Levina-Bickel where we always have either knn or epsilon neighbourhood
 
-    def __init__(self):
-        pass
-    def _fit(self, **kwargs):
-        self.dimension_pw_ = self._MLE(**kwargs)
+    Parameters
+    ----------
+    average_steps : Number of different values of k (number of neighbours) used 
+    '''
+        
+
+    def __init__(self, average_steps = 0):
+        self.average_steps = average_steps #to do: integrate averaging over k neighbourhoods
+
+    def _fit(self, X, nbhd_indices, nbhd_type, metric, radial_dists, **kwargs):
+
+        if nbhd_type == 'eps':  
+            if 'eps' in kwargs:
+                eps = kwargs['eps']
+            else:
+                eps = 1.0 #default sklearn
+        else: #knn
+            eps = None
+        
+        self.dimension_pw_ = self._MLE_basic(nbhd_type, radial_dists, eps) 
+        self.dimension_pw_ = np.array([self._mle_formula(dlist, nbhd_type, eps) for dlist in radial_dists])
         self.is_fitted_pw_ = True
-    
-    def _MLE(self, **kwargs):
-
-        pw_dims = dict()
-
-        if kwargs['nbhd_type'] == 'eps':
-            if 'radius' in kwargs:
-                R = kwargs['radius']
-            else:
-                R = 1.0 #sklearn default
-        else: #defaults to knn
-            R = None
         
-        if kwargs['metric'] == 'precomputed': 
-            D = kwargs['X']
-        else:
-            dist = DistanceMetric.get_metric(kwargs['metric'])
-            X = kwargs['X']
-
-        nbhd_dict =  kwargs['nbhd_dict']
-        for pt in nbhd_dict: #need to change dlist processing to receive dlist from above
-            if kwargs['metric'] == 'precomputed':
-                dlist = D[pt][nbhd_dict[[pt]]]
-            else:
-                dlist = dist.pairwise(X[pt], X[nbhd_dict[[pt]]])
-            pw_dims[pt] = self._mle_formula(dlist, R= R)
-        
-        return pw_dims
-
-
     @staticmethod
-    def _mle_formula(dlist, R = None):
+    def _mle_formula(dlist, nbhd_type, eps):
+
         N = len(dlist)
-        if R is None:  #knn
-            R = np.max(dlist)
-        minv = N*np.log(R) - np.sum(np.log(dlist))
+        
+        if nbhd_type == 'knn':
+            eps = np.max(dlist)
+        
+        minv = N*np.log(eps) - np.sum(np.log(dlist))
+
         if minv < 1e-9:
             return np.nan
         else:
-            if R is None: #knn
+            if nbhd_type == 'knn': 
                 return np.divide(N-1,minv)
             else: #eps
                 return np.divide(N, minv)
