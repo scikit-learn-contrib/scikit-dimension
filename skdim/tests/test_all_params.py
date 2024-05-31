bd08db6d50b1929a0dcda5e9da16c8b904fdcab7
Jakub <jakubbmalinowski@gmail.com>
2024-06-01 00:04:37 +0200

    Finish tests and GRIDE implementation

--- skdim/tests/test_all_params.py
+++ skdim/tests/test_all_params.py
@@ -213,5 +213,5 @@ def test_gride_params(data):
     x = skdim.id.Gride(n1=3, n2=8).fit(data)
     x = skdim.id.Gride(d0=1, d1=20, eps=0.001).fit(data)
     x = skdim.id.Gride(metric="minkowski").fit(data)
-    x = skdim.id.Gride.fit_transform(data)
+    x = skdim.id.Gride().fit_transform(data)
     x = skdim.id.Gride(max_range=32).fit(data).transform_multiscale()
